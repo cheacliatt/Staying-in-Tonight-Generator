3fc5b8959405c794bdcbd1ff66c973c92cc0b80a
Scott Griffin <mscottgriffin@gmail.com>
2020-06-18 14:45:27 -0400

    added script.js with rawg api

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 $(document).ready(function () {
+//genres
+  var genreArray= ['action-adventure', 'comedy', 'family-friendly', 'story-rich', 'horror']
 
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: 'https://rawg-video-games-database.p.rapidapi.com/genres',
+  // currently it is selecting a tag randomly from the array. These should instead correspond to the button ids.
+  var queryURL = 'https://api.rawg.io/api/games?tags='+ genreArray[Math.floor(Math.random() * genreArray.length)] +'&page='+[Math.floor(Math.random() * 50)] // random page number
+  $.ajax({
+    url: queryURL,
     method: 'GET',
-    headers: {
-      'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
-      'x-rapidapi-key': '97bff17986mshcf52cf710c93321p1a7692jsnaf0726e48fd1',
-    },
-  };
-
-  $.ajax(settings).done(function (response) {
+  }).then(function (response) {
     console.log(response);
-  });
-});
 
+    var gameChoice = response.results[Math.floor(Math.random() * 20)]; // random game from random page
+    console.log(gameChoice);
+    console.log(gameChoice.background_image) ///screenshot src url
+
+    //disp
+    var gameImage = gameChoice.background_image;
+    $('#screenshot').attr('src', gameImage);
+  });
+
+});
