ad3d225d91d84f585ffe77c00c8ad0361fdeff53
Scott Griffin <mscottgriffin@gmail.com>
2020-06-18 20:07:27 -0400

    testing function for games

--- game-script.js
+++ game-script.js
@@ -1,28 +1,34 @@
 $(document).ready(function () {
-//genres
+  //genres
   // var genreArray= ['action-adventure', 'comedy', 'family-friendly', 'story-rich', 'horror'] - may not need this if ids work
 
   // currently it is selecting a tag randomly from the array. These should instead correspond to the button ids.
-  var queryURL = 'https://api.rawg.io/api/games?tags='+ genreArray[Math.floor(Math.random() * genreArray.length)] +'&page='+[Math.floor(Math.random() * 50)] // random page number
-  $.ajax({
-    url: queryURL,
-    method: 'GET',
-  }).then(function (response) {
-    console.log(response);
 
-    var gameChoice = response.results[Math.floor(Math.random() * 20)]; // random game from random page
-    console.log(gameChoice);
-    console.log(gameChoice.background_image) ///screenshot src url
-
-    //disp
-    var gameImage = gameChoice.background_image;
-    $('#screenshot').attr('src', gameImage);
+  // random page number
+  $('.game-moods').on('click', function () {
+    var gameTag = $('.game-moods').val();
+    var queryURL =
+      'https://api.rawg.io/api/games?tags=' +
+      gameTag +
+      '&page=' +
+      [Math.floor(Math.random() * 50)];
+    console.log(gameTag);
+    ajaxGameCall(queryURL);
   });
 
+  function ajaxGameCall(queryURL) {
+    $.ajax({
+      url: queryURL,
+      method: 'GET',
+    }).then(function (response) {
+      console.log(response);
+
+      var gameChoice = response.results[Math.floor(Math.random() * 20)]; // random game from random page
+      console.log(gameChoice);
+      console.log(gameChoice.background_image); ///screenshot src url
+
+      var gameImage = gameChoice.background_image;
+      $('#screenshot').attr('src', gameImage);
+    });
+  }
 });
-//// Button Values (tied to movie genres, but in case there's a need to reference here)
-//comedy 35
-// action 28
-// horror 27
-// drama 18
-//family 10751
